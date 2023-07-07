8dd1e0cd9ffab771031f54c67143295049707a42
jpforevers <110951883+jpforevers@users.noreply.github.com>
2023-07-07 17:12:09 +0800

    Add clear session in delete session http interface

--- src/main/java/cloud/wangyongjun/vxmq/http/api/ApiRouterFactory.java
+++ src/main/java/cloud/wangyongjun/vxmq/http/api/ApiRouterFactory.java
@@ -33,7 +33,8 @@ public class ApiRouterFactory {
       .handler(new TestHandler(vertx));
 
     apiRouter.delete(ApiConstants.API_PREFIX_SESSION + "/:" + ModelConstants.FIELD_NAME_CLIENT_ID)
-      .handler(new DeleteSessionByClientIdHandler(vertx, ServiceFactory.sessionService(vertx), ServiceFactory.clientService(vertx)));
+      .handler(new DeleteSessionByClientIdHandler(vertx, ServiceFactory.sessionService(vertx),
+        ServiceFactory.clientService(vertx), ServiceFactory.compositeService(vertx, config)));
 
     return apiRouter;
   }
