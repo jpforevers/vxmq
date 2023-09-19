ac1545d69b3f2499f5c55f52284e245b48ae10b2
jpforevers <110951883+jpforevers@users.noreply.github.com>
2023-09-19 11:49:12 +0800

    Add vxmq.logs.level config item

--- src/main/java/cloud/wangyongjun/vxmq/VxmqLauncher.java
+++ src/main/java/cloud/wangyongjun/vxmq/VxmqLauncher.java
@@ -182,6 +182,9 @@ public class VxmqLauncher {
     ch.qos.logback.classic.Logger mqttLogger = loggerContext.getLogger("io.vertx.mqtt");
     mqttLogger.setLevel(Level.WARN);
 
+    ch.qos.logback.classic.Logger vxmqLogger = loggerContext.getLogger("cloud.wangyongjun.vxmq");
+    vxmqLogger.setLevel(Level.toLevel(Config.getLogsLevel(config)));
+
     ch.qos.logback.classic.Logger rootLogger = loggerContext.getLogger("ROOT");
     rootLogger.setLevel(Level.INFO);
     rootLogger.addAppender(consoleAppender);
