d06c0faba334986a9fbb7f073de96e7ab8d65ed9
jpforevers <110951883+jpforevers@users.noreply.github.com>
2024-07-02 15:45:58 +0800

    Optimize health check code, add HealthCheckFactory to encapsulate all relevant code

--- src/main/java/cloud/wangyongjun/vxmq/http/q/QRouterFactory.java
+++ src/main/java/cloud/wangyongjun/vxmq/http/q/QRouterFactory.java
@@ -1,38 +1,29 @@
 package cloud.wangyongjun.vxmq.http.q;
 
-import cloud.wangyongjun.vxmq.assist.Config;
 import cloud.wangyongjun.vxmq.assist.ConsumerUtil;
 import cloud.wangyongjun.vxmq.http.api.ApiConstants;
-import io.netty.handler.codec.http.HttpResponseStatus;
 import io.smallrye.mutiny.Uni;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.healthchecks.Status;
-import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.mutiny.core.MultiMap;
 import io.vertx.mutiny.core.Vertx;
 import io.vertx.mutiny.core.buffer.Buffer;
-import io.vertx.mutiny.ext.healthchecks.HealthCheckHandler;
-import io.vertx.mutiny.ext.healthchecks.HealthChecks;
 import io.vertx.mutiny.ext.web.Router;
 import io.vertx.mutiny.ext.web.RoutingContext;
-import io.vertx.mutiny.ext.web.client.WebClient;
 
 import java.util.Map;
 
 public class QRouterFactory {
-
-  private static final String HEALTH_CHECK_NAME_HTTP = "http-server";
 
   public static Router router(Vertx vertx, JsonObject config) {
     Router qRouter = Router.router(vertx);
 
     qRouter.route(ApiConstants.Q_PREFIX_PING).handler(QRouterFactory::pingHandler);
 
-    qRouter.get(ApiConstants.Q_PREFIX_HEALTH).handler(HealthCheckHandler.createWithHealthChecks(configHealthChecks(vertx, config)));
-    qRouter.get(ApiConstants.Q_PREFIX_HEALTH + "/started").handler(HealthCheckHandler.createWithHealthChecks(configStartedHealthChecks(vertx, config)));
-    qRouter.get(ApiConstants.Q_PREFIX_HEALTH + "/live").handler(HealthCheckHandler.createWithHealthChecks(configLiveHealthChecks(vertx, config)));
-    qRouter.get(ApiConstants.Q_PREFIX_HEALTH + "/ready").handler(HealthCheckHandler.createWithHealthChecks(configReadyHealthChecks(vertx, config)));
+    qRouter.get(ApiConstants.Q_PREFIX_HEALTH).handler(HealthCheckFactory.healthCheckHandler(vertx, config));
+    qRouter.get(ApiConstants.Q_PREFIX_HEALTH + "/started").handler(HealthCheckFactory.startedHealthCheckHandler(vertx, config));
+    qRouter.get(ApiConstants.Q_PREFIX_HEALTH + "/live").handler(HealthCheckFactory.liveHealthCheckHandler(vertx, config));
+    qRouter.get(ApiConstants.Q_PREFIX_HEALTH + "/ready").handler(HealthCheckFactory.readyHealthCheckHandler(vertx, config));
 
     return qRouter;
   }
@@ -58,40 +49,6 @@ public class QRouterFactory {
       jsonArray.add(jsonObject);
     }
     return jsonArray;
-  }
-
-  private static HealthChecks configHealthChecks(Vertx vertx, JsonObject config) {
-    HealthChecks hc = HealthChecks.create(vertx);
-    hc.register(HEALTH_CHECK_NAME_HTTP, 3000, checkHttpServer(vertx, config));
-    return hc;
-  }
-
-  private static HealthChecks configStartedHealthChecks(Vertx vertx, JsonObject config) {
-    HealthChecks hc = HealthChecks.create(vertx);
-    hc.register(HEALTH_CHECK_NAME_HTTP, 3000, checkHttpServer(vertx, config));
-    return hc;
-  }
-
-  private static HealthChecks configLiveHealthChecks(Vertx vertx, JsonObject config) {
-    HealthChecks hc = HealthChecks.create(vertx);
-    hc.register(HEALTH_CHECK_NAME_HTTP, 3000, checkHttpServer(vertx, config));
-    return hc;
-  }
-
-  private static HealthChecks configReadyHealthChecks(Vertx vertx, JsonObject config) {
-    HealthChecks hc = HealthChecks.create(vertx);
-    hc.register(HEALTH_CHECK_NAME_HTTP, 3000, checkHttpServer(vertx, config));
-    return hc;
-  }
-
-  private static Uni<Status> checkHttpServer(Vertx vertx, JsonObject config) {
-    return Uni.createFrom().voidItem()
-      .onItem().transformToUni(v -> {
-        WebClient webClient = WebClient.create(vertx, new WebClientOptions());
-        return webClient.get(Config.getHttpServerPort(config), "127.0.0.1", ApiConstants.Q_URL_PREFIX + ApiConstants.Q_PREFIX_PING).send()
-          .onItem().transform(bufferHttpResponse -> bufferHttpResponse.statusCode() == HttpResponseStatus.OK.code() ? Status.OK() : Status.KO())
-          .eventually(webClient::close);
-      });
   }
 
 }
