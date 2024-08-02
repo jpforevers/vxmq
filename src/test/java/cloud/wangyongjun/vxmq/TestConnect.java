dc5ffb4c4fe8fa6d822552a283ca3fd75fa1c0eb
jpforevers <wangyongjun@vip.126.com>
2024-08-02 11:27:16 +0800

    Optimize startup class VxmqLauncher and unit test class TestConnect

--- src/test/java/cloud/wangyongjun/vxmq/TestConnect.java
+++ src/test/java/cloud/wangyongjun/vxmq/TestConnect.java
@@ -1,5 +1,6 @@
 package cloud.wangyongjun.vxmq;
 
+import io.netty.handler.codec.mqtt.MqttConnectReturnCode;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import io.vertx.mqtt.MqttClientOptions;
@@ -8,21 +9,40 @@ import io.vertx.mutiny.mqtt.MqttClient;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(VertxExtension.class)
 public class TestConnect {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(TestConnect.class);
+
   @BeforeAll
   static void startServer(Vertx vertx, VertxTestContext testContext) {
-    VxmqLauncher.main(new String[]{});
+    VxmqLauncher vxmqLauncher = new VxmqLauncher();
+    vxmqLauncher.start().subscribe().with(v -> testContext.completeNow(), testContext::failNow);
   }
 
   @Test
   void testMqttSpec311Connect(Vertx vertx, VertxTestContext testContext) throws Throwable {
     MqttClientOptions mqttClientOptions = new MqttClientOptions();
-
     MqttClient mqttClient = MqttClient.create(vertx, mqttClientOptions);
-    testContext.completeNow();
+    mqttClient.connect(1883, "localhost")
+      .onItem().invoke(mqttConnAckMessage -> {
+        LOGGER.info("Mqtt client connected, code: {}", mqttConnAckMessage.code());
+        assertEquals(MqttConnectReturnCode.CONNECTION_ACCEPTED, mqttConnAckMessage.code());
+      })
+      .onItem().transformToUni(v -> mqttClient.disconnect())
+      .subscribe().with(v -> testContext.completeNow(), testContext::failNow);
+    assertTrue(testContext.awaitCompletion(2, TimeUnit.SECONDS));
+    if (testContext.failed()) {
+      throw testContext.causeOfFailure();
+    }
   }
 
 }
