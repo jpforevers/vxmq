307319a650ce674a9ea1769c26aad27d271011e0
jpforevers <wangyongjun@vip.126.com>
2024-10-04 20:00:45 +0800

    Add outbound topic alias unit test

--- src/test/java/cloud/wangyongjun/vxmq/TestPublish.java
+++ src/test/java/cloud/wangyongjun/vxmq/TestPublish.java
@@ -1,6 +1,15 @@
 package cloud.wangyongjun.vxmq;
 
 import cloud.wangyongjun.vxmq.assist.Config;
+import com.hivemq.client.mqtt.datatypes.MqttQos;
+import com.hivemq.client.mqtt.mqtt5.Mqtt5AsyncClient;
+import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
+import com.hivemq.client.mqtt.mqtt5.message.connect.Mqtt5Connect;
+import com.hivemq.client.mqtt.mqtt5.message.connect.Mqtt5ConnectRestrictions;
+import com.hivemq.client.mqtt.mqtt5.message.connect.connack.Mqtt5ConnAckReasonCode;
+import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5Publish;
+import com.hivemq.client.mqtt.mqtt5.message.subscribe.Mqtt5Subscribe;
+import com.hivemq.client.mqtt.mqtt5.message.subscribe.suback.Mqtt5SubAckReasonCode;
 import io.vertx.junit5.VertxTestContext;
 import io.vertx.mutiny.core.Vertx;
 import io.vertx.mutiny.core.buffer.Buffer;
@@ -9,10 +18,13 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestPublish extends BaseTest {
 
@@ -79,6 +91,94 @@ public class TestPublish extends BaseTest {
         return so.write(buffer);
       })
       .subscribe().with(v -> {}, testContext::failNow);
+  }
+
+  @Test
+  public void testMqtt5PubInboundTopicAlias(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    AtomicInteger atomicInteger = new AtomicInteger();
+    Mqtt5AsyncClient mqtt5SubAsyncClient = Mqtt5Client.builder()
+      .identifier("subClient")
+      .buildAsync();
+    Mqtt5AsyncClient mqtt5PubAsyncClient = Mqtt5Client.builder()
+      .identifier("pubClient")
+      .buildAsync();
+    mqtt5SubAsyncClient.connect()
+      .thenAccept(mqtt5ConnAck -> assertEquals(Mqtt5ConnAckReasonCode.SUCCESS, mqtt5ConnAck.getReasonCode()))
+      .thenCompose(v -> {
+        Mqtt5Subscribe mqtt5Subscribe = Mqtt5Subscribe.builder()
+          .topicFilter("abc/def/+")
+          .qos(MqttQos.AT_LEAST_ONCE)
+          .build();
+        return mqtt5SubAsyncClient.subscribe(mqtt5Subscribe, mqtt5Publish -> {
+          int index = atomicInteger.addAndGet(1);
+          assertTrue(mqtt5Publish.getPayload().isPresent());
+          if (index == 1) {
+            assertEquals("p1", Charset.defaultCharset().decode(mqtt5Publish.getPayload().get()).toString());
+          }
+          if (index == 2) {
+            assertEquals("p2", Charset.defaultCharset().decode(mqtt5Publish.getPayload().get()).toString());
+            testContext.completeNow();
+          }
+        });
+      })
+      .thenAccept(mqtt5SubAck -> mqtt5SubAck.getReasonCodes().forEach(mqtt5SubAckReasonCode -> assertEquals(Mqtt5SubAckReasonCode.GRANTED_QOS_1, mqtt5SubAckReasonCode)))
+      .thenCompose(v -> mqtt5PubAsyncClient.connect())
+      .thenAccept(mqtt5ConnAck -> assertEquals(Mqtt5ConnAckReasonCode.SUCCESS, mqtt5ConnAck.getReasonCode()))
+      // First PUBLISH, hivemq client will carry topic and alias
+      .thenCompose(v -> mqtt5PubAsyncClient.publish(Mqtt5Publish.builder().topic("abc/def/123").qos(MqttQos.AT_LEAST_ONCE).payload("p1".getBytes(StandardCharsets.UTF_8)).build()))
+      .thenAccept(mqtt5PublishResult -> assertTrue(mqtt5PublishResult.getError().isEmpty()))
+      // Second PUBLISH with same topic, hivemq client will only carry alias
+      .thenCompose(v -> mqtt5PubAsyncClient.publish(Mqtt5Publish.builder().topic("abc/def/123").qos(MqttQos.AT_LEAST_ONCE).payload("p2".getBytes(StandardCharsets.UTF_8)).build()))
+      .thenAccept(mqtt5PublishResult -> assertTrue(mqtt5PublishResult.getError().isEmpty()))
+      .whenComplete((v, t) -> {
+        if (t != null) {
+          testContext.failNow(t);
+        }
+      });
+  }
+
+  @Test
+  public void testMqtt5PubOutboundTopicAlias(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    AtomicInteger atomicInteger = new AtomicInteger();
+    Mqtt5AsyncClient mqtt5SubAsyncClient = Mqtt5Client.builder()
+      .identifier("subClient")
+      .buildAsync();
+    Mqtt5AsyncClient mqtt5PubAsyncClient = Mqtt5Client.builder()
+      .identifier("pubClient")
+      .buildAsync();
+    mqtt5SubAsyncClient.connect(Mqtt5Connect.builder()
+        .restrictions(Mqtt5ConnectRestrictions.builder().topicAliasMaximum(1).build())
+        .build())
+      .thenAccept(mqtt5ConnAck -> assertEquals(Mqtt5ConnAckReasonCode.SUCCESS, mqtt5ConnAck.getReasonCode()))
+      .thenCompose(v -> {
+        Mqtt5Subscribe mqtt5Subscribe = Mqtt5Subscribe.builder()
+          .topicFilter("abc/def/+")
+          .qos(MqttQos.AT_LEAST_ONCE)
+          .build();
+        return mqtt5SubAsyncClient.subscribe(mqtt5Subscribe, mqtt5Publish -> {
+          int index = atomicInteger.addAndGet(1);
+          assertTrue(mqtt5Publish.getPayload().isPresent());
+          if (index == 1) {
+            assertEquals("p1", Charset.defaultCharset().decode(mqtt5Publish.getPayload().get()).toString());
+          }
+          if (index == 2) {
+            assertEquals("p2", Charset.defaultCharset().decode(mqtt5Publish.getPayload().get()).toString());
+            testContext.completeNow();
+          }
+        });
+      })
+      .thenAccept(mqtt5SubAck -> mqtt5SubAck.getReasonCodes().forEach(mqtt5SubAckReasonCode -> assertEquals(Mqtt5SubAckReasonCode.GRANTED_QOS_1, mqtt5SubAckReasonCode)))
+      .thenCompose(v -> mqtt5PubAsyncClient.connect())
+      .thenAccept(mqtt5ConnAck -> assertEquals(Mqtt5ConnAckReasonCode.SUCCESS, mqtt5ConnAck.getReasonCode()))
+      .thenCompose(v -> mqtt5PubAsyncClient.publish(Mqtt5Publish.builder().topic("abc/def/123").qos(MqttQos.AT_LEAST_ONCE).payload("p1".getBytes(StandardCharsets.UTF_8)).build()))
+      .thenAccept(mqtt5PublishResult -> assertTrue(mqtt5PublishResult.getError().isEmpty()))
+      .thenCompose(v -> mqtt5PubAsyncClient.publish(Mqtt5Publish.builder().topic("abc/def/123").qos(MqttQos.AT_LEAST_ONCE).payload("p2".getBytes(StandardCharsets.UTF_8)).build()))
+      .thenAccept(mqtt5PublishResult -> assertTrue(mqtt5PublishResult.getError().isEmpty()))
+      .whenComplete((v, t) -> {
+        if (t != null) {
+          testContext.failNow(t);
+        }
+      });
   }
 
 }
